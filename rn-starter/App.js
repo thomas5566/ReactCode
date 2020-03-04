159ec42fea331f737f608041e8489483c6912f1f
Thomas5566 <fain73@hotmail.com>
2020-03-04 08:51:14 +0800

    2020/03/03

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -7,6 +7,7 @@ import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from './src/screens/CounterScreen';
 import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
+import MainProject from './src/screens/MainProject';
 
 const navigator = createStackNavigator(
   {
@@ -17,9 +18,10 @@ const navigator = createStackNavigator(
     Counter: CounterScreen,
     Color: ColorScreen,
     Square: SquareScreen,
+    Project: MainProject,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Project",
     defaultNavigationOptions: {
       title: "App"
     }
